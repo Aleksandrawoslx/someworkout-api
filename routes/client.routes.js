f5e1e64c001a98c16d6ec78e5287987d9c062b75
Aleksandra Wos <aleksandra1.wos@gmail.com>
2022-03-08 09:44:47 +0000

    commit

---
+++ routes/client.routes.js
@@ -0,0 +1,48 @@
+const router = require("express").Router();
+const Client = require("../models/Client.model");
+const mongoose = require("mongoose")
+const {isAuthenticated} = require("../middleware/jwt.middleware");
+const req = require("express/lib/request");
+const { route } = require("./auth.routes");
+
+
+router.get("/", isAuthenticated, (req, res, next)=>{
+  Wod.find()
+  .then(allWods => res.json(allWods))
+  .catch(err=> res.json(err));
+})
+
+router.post("/", isAuthenticated, (req,res, next)=>{
+
+  const wodDetails = {
+    name: req.body.name,
+    type: req.body.type,
+    description: req.body.description,
+    rounds: req.body.rounds,
+    workout: req.body.workouts,
+    tags: req.body.tags
+  }
+
+  Wod.create(wodDetails)
+  .then(wodMade =>{
+    res.status(201).json(wodMade)
+  })
+  .catch(err=>{
+    console.log("workout error try again", err);
+    res.status(500).json({
+      message: "error creating a wod",
+      error: err
+    });
+  })
+})
+
+router.get("/:wodId", isAuthenticated, (req,res,next)=>{
+  const {wodId} = req.params; 
+
+  Wod.findById(wodId)
+  .then(wod => res.json(wod))
+  .catch(err => res.status(500).json(err))
+})
+
+module.exports = router;
+
